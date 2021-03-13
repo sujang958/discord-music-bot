b6ff280de6ce87aaef7c212634f0dc900eec2122
sujang958 <enter7377@naver.com>
2021-03-13 15:47:24 +0900

    Migrate To functions/music

--- functions/music.js
+++ functions/music.js
@@ -231,5 +231,19 @@ module.exports = {
 
         queue.dispatcher.setVolume(volume / 100);
         return message.reply('설정했어요!');
+    },
+    /**
+     * @param {Map} queue 
+     * @param {import('discord.js').Message} message 
+     */
+    async resume(queue, message) {
+        if (!message.member.voice)  return message.reply('음성채널에 들어가있어야 해요!');
+        let queue = client.queue.get(`${message.guild.id}`);
+        if (queue.musics.length <= 0) return message.reply('재생중인 음악이 없어요!');
+        if (!queue.dispatcher)  return message.reply('재생중인 음악이 없어요!');
+        if (!queue.dispatcher.paused) return message.reply('이미 재생중이에요!');
+        
+        queue.dispatcher.resume();
+        message.reply('노래를 재생할께요!');
     }
 }

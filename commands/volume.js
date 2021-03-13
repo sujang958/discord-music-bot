91bc082f0b6ea026964b8ace75066bbca0a49058
sujang958 <enter7377@naver.com>
2021-03-13 15:02:37 +0900

    Migrate To functions/music

--- commands/volume.js
+++ commands/volume.js
@@ -10,15 +10,6 @@ module.exports = {
      * @param {Array} args 
      */
     async run(client, message, args) {
-        if (!message.member.voice)  return message.reply('음성채널에 들어가있어야 해요!');
-        let queue = client.queue.get(`${message.guild.id}`);
-        if (queue.musics.length <= 0) return message.reply('재생중인 음악이 없어요!');
-        if (!queue.dispatcher)  return message.reply('재생중인 음악이 없어요!');
-        if (isNaN(args[0])) return message.reply('숫자를 입력해야돼요!');
-        let volume = Number(args[0]);
-        if (volume < 0.9 || volume > 100)   return message.reply('1 ~ 100을 입력해야돼요!');
-
-        queue.dispatcher.setVolume(volume / 100);
-        return message.reply('설정했어요!');
+        client.music.volume(queue, message);
     }
 }
--- functions/music.js
+++ functions/music.js
@@ -180,5 +180,56 @@ module.exports = {
         }
         
         message.reply({embed: embedJSON});
+    },
+    /**
+     * @param {Map} queue 
+     * @param {import('discord.js').Message} message 
+     */
+    async skip(queue, message) {
+        if (!message.member.voice)  return message.reply('음성채널에 들어가있어야 해요!');
+        if (!queue.has(message.guild.id)) return message.reply('재생중인 음악이 없어요!');
+        queue = queue.get(`${message.guild.id}`);
+        if (queue.musics.length <= 0) return message.reply('재생중인 음악이 없어요!');
+        if (!queue.dispatcher)  return message.reply('재생중인 음악이 없어요!');
+
+        if (queue.musics[0].user == message.author.id) {
+            queue.dispatcher.end();
+            return message.reply('스킵했어요!');    
+        } else {
+            let chkMsg = await message.channel.send('스킵 찬반 투표, 아래 이모지를 통화방의 모든 인원이 눌렀을때 스킵돼요!');
+            chkMsg.react('✅');
+            let memberCount = message.member.voice.channel.members.size;
+            let memberArr = message.member.voice.channel.members.array();
+            let userIDs = [];
+            for (let i in memberArr) {
+                userIDs.push(memberArr[i].id);
+            }
+            let collector = chkMsg.createReactionCollector((reaction, user) => reaction.emoji.name == "✅" &&  userIDs.includes(user.id), {tiem: 30000});
+
+            collector.on('collect', reaction => {
+                if (reaction.count == memberCount) {
+                    message.channel.send('스킵했어요!');
+                    return queue.dispatcher.end();
+                }     
+            });
+        }
+    },
+    /**
+     * @param {Map} queue 
+     * @param {import('discord.js').Message} message 
+     */
+    async volume(queue, message) {
+        if (!message.member.voice)  return message.reply('음성채널에 들어가있어야 해요!');
+        if (!queue.has(message.guild.id)) return message.reply('재생중인 음악이 없어요!');
+        queue = queue.get(`${message.guild.id}`);
+        if (!queue.has('musics')) return message.reply('재생중인 음악이 없어요!');
+        if (queue.musics.length <= 0) return message.reply('재생중인 음악이 없어요!');
+        if (!queue.dispatcher)  return message.reply('재생중인 음악이 없어요!');
+        if (isNaN(args[0])) return message.reply('숫자를 입력해야돼요!');
+        let volume = Number(args[0]);
+        if (volume < 0.9 || volume > 100)   return message.reply('1 ~ 100을 입력해야돼요!');
+
+        queue.dispatcher.setVolume(volume / 100);
+        return message.reply('설정했어요!');
     }
 }
